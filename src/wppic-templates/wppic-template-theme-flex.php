8659c14fc2ebd085c3959247c3194be8c0679c91
Ronald Huereca <rhuereca@updraftplus.com>
2019-05-16 07:49:26 -0500

    Added flex theme layout.

--- src/wppic-templates/wppic-template-theme-flex.php
+++ src/wppic-templates/wppic-template-theme-flex.php
@@ -21,9 +21,21 @@ if( !empty( $image ) ){
 ?>
 <div class="wp-pic-flip" style="display: none;">
 	<div class="wp-pic-face wp-pic-front">
-		<a class="wp-pic-logo" href="<?php echo $wppic_data->url ?>" <?php echo $bgImage ?> target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>"></a>
-		<a class="wp-pic-name" href="<?php echo $wppic_data->url ?>" target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>"><?php echo $wppic_data->name ?></a>
-		<p class="wp-pic-author"><?php _e( 'Author(s):', 'wp-plugin-info-card' ) ?> <?php echo $wppic_data->author ?></p>
+		<div class="wp-pic-banner-wrapper">
+			<a href="<?php echo $wppic_data->url ?>" target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>"><img src="<?php echo esc_url( $wppic_data->screenshot_url ); ?>" alt="<?php echo esc_attr( $wppic_data->name ); ?>" /></a>
+		</div>
+		<div class="wp-pic-name-wrapper">
+			<a class="wp-pic-name" href="<?php echo $wppic_data->url ?>" target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>"><strong><?php echo $wppic_data->name ?></strong></a>
+		</div>
+		<div class="wp-pic-preview-wrapper">
+			<a class="wp-pic-preview" href="<?php echo $wppic_data->preview_url ?>" title="<?php _e( 'Theme Preview', 'wp-plugin-info-card' ) ?>" target="_blank"><span><?php _e( 'Theme Preview', 'wp-plugin-info-card' ) ?></span></a>
+		</div>
+		<div class="wp-pic-updated-wrapper">
+			<p class="wp-pic-updated"><span><?php _e( 'Last Updated:', 'wp-plugin-info-card' ) ?></span> <?php echo $wppic_data->last_updated ?></p>
+		</div>
+		<div class="wp-pic-author-wrapper">
+			<p class="wp-pic-author"><?php _e( 'Author(s):', 'wp-plugin-info-card' ) ?> <?php echo $wppic_data->author ?></p>
+		</div>
 		<div class="wp-pic-bottom">
 			<div class="wp-pic-bar">
 				<a href="https://wordpress.org/support/view/theme-reviews/<?php echo $wppic_data->slug ?>" class="wp-pic-rating" target="_blank" title="<?php _e( 'Ratings', 'wp-plugin-info-card' ) ?>">
@@ -36,35 +48,12 @@ if( !empty( $image ) ){
 					<?php echo $wppic_data->version ?><em><?php _e( 'Version', 'wp-plugin-info-card' ) ?></em>
 				</a>
 			</div>
-			<div class="wp-pic-download">
-				<span><?php _e( 'More info', 'wp-plugin-info-card' ) ?></span>
-			</div>
-		</div>
-	</div>
-	<div class="wp-pic-face wp-pic-back">
-		<a class="wp-pic-dl-ico" href="<?php echo $wppic_data->download_link ?>" title="<?php _e( 'Direct download', 'wp-plugin-info-card' ) ?>"></a>
-		<p><a class="wp-pic-dl-link" href="<?php echo $wppic_data->download_link ?>" title="<?php _e( 'Direct download', 'wp-plugin-info-card' ) ?>"><?php echo basename($wppic_data->download_link) ?></a></p>
-		<a class="wp-pic-preview" href="<?php echo $wppic_data->preview_url ?>" title="<?php _e( 'Theme Preview', 'wp-plugin-info-card' ) ?>" target="_blank"><span><?php _e( 'Theme Preview', 'wp-plugin-info-card' ) ?></span></a>
-		<p class="wp-pic-updated"><span><?php _e( 'Last Updated:', 'wp-plugin-info-card' ) ?></span> <?php echo $wppic_data->last_updated ?></p>
-		<div class="wp-pic-bottom">
-			<div class="wp-pic-bar">
-				<a href="https://wordpress.org/support/view/theme-reviews/<?php echo $wppic_data->slug ?>" class="wp-pic-rating" target="_blank" title="<?php _e( 'Ratings', 'wp-plugin-info-card' ) ?>">
-					<?php echo $wppic_data->rating ?>%<em><?php _e( 'Ratings', 'wp-plugin-info-card' ) ?></em>
-				</a>
-				<a href="<?php echo $wppic_data->download_link ?>" class="wp-pic-downloaded" target="_blank" title="<?php _e( 'Direct download', 'wp-plugin-info-card' ) ?>">
-					<?php echo number_format( $wppic_data->active_installs ); ?><em><?php _e( 'Active Installs', 'wp-plugin-info-card' ) ?></em>
-				</a>
-				<a href="<?php echo $wppic_data->url ?>" class="wp-pic-version" target="_blank" title="<?php _e( 'WordPress.org Plugin Page', 'wp-plugin-info-card' ) ?>">
-					<?php echo $wppic_data->version ?><em><?php _e( 'Version', 'wp-plugin-info-card' ) ?></em>
+			<div class="wp-pic-download-link">
+				<a href="<?php echo esc_url( $wppic_data->download_link ) ?>" target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>">
+				<span><?php _e( 'Download:', 'wp-plugin-info-card' ); ?> <?php echo esc_html( $wppic_data->name ); ?></span>
 				</a>
 			</div>
-			<a class="wp-pic-page" href="<?php echo $wppic_data->url ?>" target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>"><?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?></a>
 		</div>
-		<a class="wp-pic-asset-bg" <?php echo $bgImage ?> href="<?php echo $wppic_data->url ?>" target="_blank" title="<?php _e( 'WordPress.org Theme Page', 'wp-plugin-info-card' ) ?>">
-			<span class="wp-pic-asset-bg-title"><span><?php echo $wppic_data->name ?></span></span>
-		</a>
-		<div class="wp-pic-goback" title="<?php _e( 'Back', 'wp-plugin-info-card' ) ?>"><span></span></div>
-		<?php echo $wppic_data->credit ?>
 	</div>
 </div>
 <?php //end of template
