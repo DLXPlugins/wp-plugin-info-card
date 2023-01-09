1ddda75eecbb2be06988ac95a7829dd8743d2f0d
Ronald Huereca <ronald@mediaron.com>
2023-01-09 08:38:11 -0600

    Moving dashboard widgets to admin.

--- functions.php
+++ functions.php
@@ -25,7 +25,7 @@ $wppicDateFormat = get_option( 'date_format' );
 /***************************************************************
  * Load plugin files
  ***************************************************************/
-$wppicFiles = array( 'api','shortcode','widget', 'add-plugin', 'add-theme', 'query' );
+$wppicFiles = array( 'api','shortcode','add-plugin', 'add-theme', 'query' );
 foreach( $wppicFiles as $wppicFile ){
 	require_once( WPPIC_PATH . 'wp-plugin-info-card-' . $wppicFile . '.php' );
 }
--- php/Admin.php
+++ php/Admin.php
@@ -23,7 +23,9 @@ class Admin {
 		add_action( 'admin_enqueue_scripts', array( $self, 'enqueue_admin_scripts' ) );
 		add_action( 'admin_menu', array( $self, 'add_options_page' ) );
 		add_action( 'admin_init', array( $self, 'register_settings' ) );
-		add_action( 'wp_ajax_async_wppic_clear_cache', array( $self, 'clear_cache' ) );
+		add_action( 'wp_ajax_async_wppic_clear_cache', array( $self, 'ajax_clear_cache' ) );
+		add_action( 'wp_ajax_wppic_widget_render', array( $self, 'ajax_dashboard_widget_render' ) );
+		add_action( 'wp_dashboard_setup', array( $self, 'add_dashboard_widgets' ) );
 		return $self;
 	}
 
@@ -33,7 +35,8 @@ class Admin {
 	 * @param string $hook The current admin page.
 	 */
 	public function enqueue_admin_scripts( $hook ) {
-		if ( 'settings_page_wp-plugin-info-card' !== $hook ) {
+		// Bail if we're not on the plugin's admin page or the dashboard page.
+		if ( 'settings_page_wp-plugin-info-card' !== $hook && 'index.php' !== $hook ) {
 			return;
 		}
 
@@ -485,10 +488,148 @@ class Admin {
 	/**
 	 * Clear Plugin cache.
 	 */
-	public function clear_cache() {
+	public function ajax_clear_cache() {
 		if ( ! current_user_can( 'manage_options' ) ) {
 			return;
 		}
 		wppic_delete_transients();
+	}
+
+	/**
+	 * Adds a dashboard widget to the dashboard.
+	 */
+	public function add_dashboard_widgets() {
+		$options = Options::get_options();
+
+		// Ensure widget option is valid boolean.
+		$options_widget = $options['widget'] ?? false;
+		$options_widget = filter_var( $options_widget, FILTER_VALIDATE_BOOLEAN );
+
+		// Get SVG.
+		$wppic_svg = Functions::get_plugin_url( 'assets/img/wppic.svg' );
+		if ( true === $options_widget ) {
+			wp_add_dashboard_widget(
+				'wppic-dashboard-widget',
+				'<img src="' . esc_url( $wppic_svg ) . '" class="wppic-logo" alt="b*web" style="display:none"/>&nbsp;&nbsp;' . Functions::get_plugin_name() . ' board',
+				array( $this, 'output_dashboard_widgets' )
+			);
+		}
+	}
+
+	/**
+	 * Output dashboard widgets.
+	 */
+	public function output_dashboard_widgets() {
+		$options    = Options::get_options();
+		$list_state = false;
+		$ajax_class = '';
+
+		if ( isset( $options['ajax'] ) && true === $options['ajax'] ) {
+			$ajax_class = 'ajax-call';
+		}
+
+		$wppic_types = array();
+		$wppic_types = apply_filters( 'wppic_add_widget_type', $wppic_types );
+
+		$content = '<div class="wp-pic-list ' . $ajax_class . '">';
+
+		foreach ( $wppic_types as $wppic_type ) {
+
+			$rows = array();
+
+			if ( isset( $options[ $wppic_type[1] ] ) && ! empty( $options[ $wppic_type[1] ] ) ) {
+
+				$list_state  = true;
+				$other_lists = false;
+
+				foreach ( $wppic_types as $wppic_list ) {
+					if ( $wppic_type[1] !== $wppic_list[1] ) {
+						$rows[] = $wppic_list[1];
+					}
+				}
+
+				foreach ( $rows as $row ) {
+					if ( isset( $options[ $row ] ) && ! empty( $options[ $row ] ) ) {
+						$other_lists = true;
+					}
+				}
+
+				if ( $other_lists ) {
+					$content .= '<h4>' . $wppic_type[2] . '</h4>';
+				}
+
+				if ( isset( $options['ajax'] ) && true === $options['ajax'] ) {
+					$content .= '<div class="wp-pic-loading" style="background-image: url( ' . admin_url() . 'images/spinner-2x.gif);" data-type="' . $wppic_type[0] . '" data-list="' . htmlspecialchars( json_encode( ( $options[ $wppic_type[1] ] ) ), ENT_QUOTES, 'UTF-8' ) . '"></div>';
+				} else {
+					$content .= $this->render_widget( $wppic_type[0], $options[ $wppic_type[1] ] );
+				}
+			}
+		}
+
+		// Nothing found.
+		if ( ! $list_state ) {
+
+			$content .= '<div class="wp-pic-item" style="display:block;">';
+			$content .= '<span class="wp-pic-no-item"><a href="admin.php?page=' . WPPIC_ID . '">' . __( 'Nothing found, please add at least one item in the WP Plugin Info Card settings page.', 'wp-plugin-info-card' ) . '</a></span>';
+			$content .= '</div>';
+
+		}
+
+		$content .= '</div>';
+
+		// todo - snaitize.
+		echo $content;
+
+	}
+
+	/**
+	 * Renders a widget via Ajax.
+	 */
+	public function ajax_dashboard_widget_render() {
+		$type  = filter_input( INPUT_POST, 'wppic-type', FILTER_DEFAULT );
+		$list  = filter_input( INPUT_POST, 'wppic-list', FILTER_DEFAULT );
+		$slugs = array( sanitize_text_field( $list ) );
+
+		$content = $this->render_widget( $type, $slugs );
+
+		if ( ! empty( $list ) ) {
+			// todo - snaitize.
+			echo $content;
+		} else {
+			return $content;
+		}
+		exit;
+	}
+
+	/**
+	 * Render a dashboard widget item.
+	 *
+	 * @param string $type  Type of the item (plugins, themes).
+	 * @param array  $slugs Slug of the item.
+	 *
+	 * @return string $content.
+	 */
+	private function render_widget( $type, $slugs ) {
+		$content = '';
+
+		if ( ! empty( $slugs ) ) {
+			foreach ( $slugs as $slug ) {
+				$wppic_data = wppic_api_parser( $type, $slug, '5', 'widget' );
+
+				if ( ! $wppic_data ) {
+
+					$content .= '<div class="wp-pic-item ' . $slug . '">';
+					$content .= '<span class="wp-pic-no-item">' . esc_html__( 'Item not found:', 'wp-plugin-info-card' ) . ' "' . $slug . '" ' . esc_html__( 'does not exist.', 'wp-plugin-info-card' ) . '</span>';
+					$content .= '</div>';
+
+				} else {
+
+					$content = apply_filters( 'wppic_add_widget_item', $content, $wppic_data, $type );
+
+				}
+			}
+		}
+
+		return $content;
 	}
 }
