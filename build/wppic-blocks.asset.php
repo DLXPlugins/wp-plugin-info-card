9bdf5c55838464820d60bfee221508718b488c97
Ronald Huereca <ronald@mediaron.com>
2023-12-12 09:15:36 -0600

    Auto stash before merge of "dev" and "origin/dev"

--- build/wppic-blocks.asset.php
+++ build/wppic-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-blocks', 'wp-element', 'wp-url'), 'version' => '79b40106a5b386bdd05c');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-blocks', 'wp-element', 'wp-url'), 'version' => 'c12f4e2bbca2dfb876c8');
