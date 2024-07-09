6ae8a33c5b4371a242ff106730512de4ddb64ed3
Ronald Huereca <ronald@mediaron.com>
2024-07-09 13:31:55 -0500

    Doing prod build.

--- build/wppic-blocks.asset.php
+++ build/wppic-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '96407c343610de227b7e');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1584fb2891cfb26e5c95');
