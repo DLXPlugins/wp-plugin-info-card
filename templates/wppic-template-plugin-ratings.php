4e9006716b504f319eb3c437afaa7be5035db9a6
Ronald Huereca <ronald@mediaron.com>
2024-07-09 04:59:01 -0500

    Adding filter to reduce words in title.

--- templates/wppic-template-plugin-ratings.php
+++ templates/wppic-template-plugin-ratings.php
@@ -61,7 +61,18 @@ if ( ! empty( $wppic_data->banners['low'] ) ) {
 				</span>
 			</div>
 		</div>
-		<div class="wp-pic-name"><?php echo esc_html( wp_trim_words( $wppic_data->name, 7 ) ); ?></div>
+		<?php
+		/**
+		 * Filter the number of words to trim the plugin name.
+		 *
+		 * @param int $plugin_trim_words Number of words to trim the plugin name.
+		 * @param object $wppic_data Plugin data.
+		 *
+		 * @since 5.1.0
+		 */
+		$plugin_trim_words = apply_filters( 'wppic_plugin_trim_words', 7, $wppic_data );
+		?>
+		<div class="wp-pic-name"><?php echo esc_html( wp_trim_words( $wppic_data->name, $plugin_trim_words ) ); ?></div>
 		<?php /* Translators: %d is the number of ratings for a plugin */ ?>
 		<div class="wp-pic-rating-stats"><?php echo esc_html( round( $wppic_data->rating / 20, 1 ) ); ?> <?php printf( __( 'stars based on %s ratings', 'wp-plugin-info-card' ), number_format_i18n( absint( $wppic_data->num_ratings ) ) ); ?></div>
 		<div class="wp-pic-bottom wp-pic-bottom-ratings">
