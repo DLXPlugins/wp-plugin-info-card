db76f0c3e0a0c8e8500be1f39c88547ab8bc897d
Ronald Huereca <ronald@mediaron.com>
2024-07-17 08:36:52 -0500

    Doing prod build.

--- build/wppic-blocks.asset.php
+++ build/wppic-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '80afdfe79911f877909b');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1584fb2891cfb26e5c95');
--- dist/wppic-admin-home.asset.php
+++ dist/wppic-admin-home.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'ba908d79e0c03231dac2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '0aafa07b8d421b6f6e58');
