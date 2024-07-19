cfa10ee8fed24a51b614fcadcc82d06ca6a56189
Ronald Huereca <ronald@mediaron.com>
2024-07-19 03:47:09 -0500

    Adding commands for adding a plugin or theme.

--- build/wppic-blocks.asset.php
+++ build/wppic-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1584fb2891cfb26e5c95');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-commands', 'wp-components', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '84c2ec11de3c423aa040');
