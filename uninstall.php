0317ace21983f344ed60abfc240f1a00649095f8
Ronald Huereca <ronald@mediaron.com>
2025-06-02 13:20:16 -0500

    Fixing uninstall fatal.

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,11 @@
 if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
+/**
+ * Include global functions and variables.
+ */
+require_once 'functions.php';
+
 delete_option( 'wppic_settings' );
 wp_clear_scheduled_hook( 'wppic_daily_cron' );
 wppic_delete_transients();
