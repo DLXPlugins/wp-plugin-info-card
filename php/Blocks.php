d3f4b922f41e0752c7ae8255f939c53fa49baf73
Ronald Huereca <ronald@mediaron.com>
2025-07-31 07:20:35 -0500

    Adding render callbacks.

--- php/Blocks.php
+++ php/Blocks.php
@@ -284,25 +284,37 @@ class Blocks {
 	 */
 	public function register_blocks() {
 
-		if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
-			wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
-			return;
-		} else {
-			if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
-				wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
-			}
-			$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
-			foreach ( array_keys( $manifest_data ) as $block_type ) {
-				register_block_type( __DIR__ . "/build/{$block_type}" );
-			}
-		}
-
 		$render_callbacks = array(
 			'wp-plugin-info-card/wp-plugin-info-card'   => array( $this, 'info_card_render' ),
 			'wp-plugin-info-card/wp-plugin-info-card-query' => array( $this, 'info_card_query_render' ),
 			'wp-plugin-info-card/site-plugin-card-grid' => array( $this, 'site_plugin_card_grid_render' ),
 			'wp-plugin-info-card/plugin-screenshots-info-card' => array( $this, 'site_plugin_screenshots' ),
 		);
+
+		add_filter(
+			'block_type_metadata_settings',
+			function ( $settings, $metadata ) use ( $render_callbacks ) {
+				if ( isset( $render_callbacks[ $metadata['name'] ] ) ) {
+					$settings['render_callback'] = $render_callbacks[ $metadata['name'] ];
+				}
+				return $settings;
+			},
+			10,
+			2
+		);
+
+		if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
+			wp_register_block_types_from_metadata_collection( Functions::get_plugin_dir( 'build' ), Functions::get_plugin_dir( 'build/blocks-manifest.php' ) );
+			return;
+		} else {
+			if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
+				wp_register_block_metadata_collection( Functions::get_plugin_dir( 'build' ), Functions::get_plugin_dir( 'build/blocks-manifest.php' ) );
+			}
+			$manifest_data = require Functions::get_plugin_dir( 'build/blocks-manifest.php' );
+			foreach ( array_keys( $manifest_data ) as $block_type ) {
+				register_block_type( __DIR__ . "/build/{$block_type}" );
+			}
+		}
 	}
 
 	/**
