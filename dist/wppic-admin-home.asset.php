69a2f012a2e29f91a3f9a0ea191156b5a60bcebb
Ronald Huereca <ronald@mediaron.com>
2025-12-22 23:21:47 -0600

    Removing dynamic reference.

--- dist/wppic-admin-home.asset.php
+++ dist/wppic-admin-home.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'e634d2533e219b56c61d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'd606cd4838e0681ad478');
