134545b162be3bf1cee0f150408c8d1d24e938bf
Ronald Huereca <ronald@mediaron.com>
2025-12-27 03:38:07 -0600

    Doing prod build.

--- build/edd-sidebar.asset.php
+++ build/edd-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2996f7e13237dc60bb4d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'a24eb8bf6376f7b48279');
